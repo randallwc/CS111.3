32945dd20f3055c6a0a51008b1efeb7b8b3625df
Dylan Gunn <dylankgunn@gmail.com>
2020-12-03 01:48:47 -0800

    updated lab3a.cpp main

--- working/lab3a.cpp
+++ working/lab3a.cpp
@@ -7,8 +7,47 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "ext2_fs.h"
+using namespace std
+
+struct ext2_super_block superblock;
+
+int blocksize;
 
 int main(int argc, char** argc)
 {
+    if (argc != 2)
+        fprintf(stderr, "Error: Incorrect usage. Expected: ./lab3a [image]\n");
+
+    img = open(argv[1], O_RDONLY);
+    if (img == -1)
+    {
+        fprintf(stderr, "Error: Unable to open the specified file");
+        exit(1);
+    }
+    
+    if (pread(img, &superblock; sizeof(struct ext2_super_block), SUPERBLOCK_OFFSET) != sizeof(struct ext2_super_block))
+    {
+        fprintf(stderr, "Error: Encountered an issue on pread() for superblock\n");
+        exit(1);
+    }
+    if (superblock.s_magic != EXT2_SUPER_MAGIC)
+    {
+        fprintf(stderr, "Error: File system is not EXT2\n");
+        exit(1);
+    }
+
+    blocksize = EXT2_MIN_BLOCK_SIZE << superblock.s_log_block_size;
+
+    cout << "SUPERBLOCK," 
+        << superblock.s_blocks_count << ','
+        << superblock.s_inodes_count << ','
+        << blocksize << ','
+        << superblock.s_inode_size << ','
+        << superblock.s_blocks_per_group << ','
+        << superblock.s_inodes_per_group << ','
+        << superblock.s_first_ino << endl;
+
+    
+
     return 0;
 }
