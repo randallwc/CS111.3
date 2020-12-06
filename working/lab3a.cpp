ba0e08d5a440b199e45cb8a93763dd164a8a5fa9
William Randall <wrandall1000@gmail.com>
2020-12-05 23:23:19 -0600

    cout

--- working/lab3a.cpp
+++ working/lab3a.cpp
@@ -201,8 +201,8 @@ void freeiNodeBitmap(int index, int off, int table, __u32 numBytes)
         for (unsigned int j = 0; j < 8; j++)
         {
             if (!(int)(bit & 1))
-                // cout << "IFREE," << iNodeNum << endl; // number of the free I-node (decimal)
-                printf("IFREE,%d\n", iNodeNum);
+                cout << "IFREE," << (int)iNodeNum << endl; // number of the free I-node (decimal)
+                // printf("IFREE,%d\n", iNodeNum);
             else
                iNodeSummary(table, iNodeNum);
 
