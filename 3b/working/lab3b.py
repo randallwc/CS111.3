15ba0aef3561fc1792471ae2463e3e79a10a5758
William Randall <wrandall1000@gmail.com>
2020-12-10 02:36:12 -0600

    started check_blocks

--- 3b/working/lab3b.py
+++ 3b/working/lab3b.py
@@ -4,14 +4,19 @@
 
 import csv
 import sys
+import math
 
-# import math
-
+# GLOBAL LISTS
 bfree = []
 ifree = []
 
+# GLOBAL ERROR FLAG
+error_flag = False
 
-class Superblock:  # do dictionary entries for each of these?
+
+# TODO -- do dictionary entries for each of these?
+# CLASS DEFINITIONS
+class Superblock:
     def __init__(self, row):
         self.num_blocks = int(row[1])
         self.num_inodes = int(row[2])
@@ -26,7 +31,7 @@ class Group:
     def __init__(self, row):
         self.num = int(row[1])
         self.inode_per_group = int(row[3])
-        self.inode_tb = int(row[8])
+        self.inode_table = int(row[8])
 
 
 class Inode:
@@ -67,40 +72,113 @@ class Indirect:
         self.ref_block_num = int(row[5])
 
 
-def scan_blocks():
-    pass
+def check_blocks(inodes, dirents, indirects, initial_block, num_blocks):
+    blocks_allocated_d = {}
+    duplicate_blocks = set()
+    offsets = {1: 12, 2: 268, 3: 65804}
+    depths = {0: '', 1: ' INDIRECT', 2: ' DOUBLE INDIRECT', 3: ' TRIPLE INDIRECT'}
+
+    def analyze_direct_block(offset_t, block_t, inode_t, num_blocks_t) -> int:
+        global error_flag
+        num = inode_t.num
+        # if the block is out of range
+        if block_t >= num_blocks_t or block_t < 0:
+            print(f'INVALID BLOCK {block_t} IN INODE {num} AT OFFSET {offset_t}')
+            error_flag = True
+        # if block is from 0 to the first block that block is reserved
+        elif 0 < block_t < initial_block:
+            print(f"RESERVED BLOCK {block_t} IN INODE {num} AT OFFSET {offset_t}")
+            error_flag = True
+        elif block_t == 0:
+            pass  # TODO -- what to if block == 0
+        # if block is not 0 add it to the allocated dictionary
+        else:
+            element = [num, offset_t, 0]
+            if block_t not in blocks_allocated_d:
+                blocks_allocated_d[block_t] = [element] # list of lists
+            else:
+                duplicate_blocks.add(block_t)
+                blocks_allocated_d[block_t].append(element)
+        return offset_t + 1
+
+    def analyze_indirect_block(offset_t, block_t, inode_t, num_blocks_t, depth_t) -> int:
+        global error_flag
+        num = inode_t.num
+        off = offsets[depth_t]
+        depth_str = depths[depth_t]
+        if num_blocks_t:
+            pass
+
+        return depth + 1
+
+    def analyze_indirect_block():
+        global error_flag
+        pass
+
+    def find_duplicate_blocks():
+        global error_flag
+        pass
+
+    def analyze_blocks():
+        global error_flag
+        pass
+
+    # look at each inode
+    for inode in inodes:
+        # skip soft links and links of size less than or equal to 60
+        if inode.file_type is 's' and inode.file_size <= 60:
+            continue
+        # look at each direct block
+        offset = 0
+        for block in inode.directories:
+            offset = analyze_direct_block(offset, block, inode, num_blocks)
+        # look at each indirect block
+        depth = 1
+        for block in inode.indirect_refs:
+            depth = analyze_indirect_block(offset, block, inode, num_blocks, depth)
+
+    analyze_indirect_blocks()
+    find_duplicate_blocks()
+    analyze_blocks()
 
 
-def scan_inodes(inodes, dirents, first, cap):
+# HELPER FUNCTION DEFINITIONS
+def check_inodes(inodes, dirents, first, cap):
     allocated = []  # TODO -- name?
     link_count = {}
     parent_dir = {}
 
     def check_freelist(inodes_t, alloc):
+        global error_flag
         for inode in inodes_t:
             num = inode.num
             if num != 0:
                 alloc.append(num)
                 if num in ifree:
                     print(f"ALLOCATED INODE {num} ON FREELIST")
+                    error_flag = True
             if inode.file_type == '0' and num not in ifree:
                 print(f"UNALLOCATED INODE {num} NOT ON FREELIST")
+                error_flag = True
         for num in range(first, cap):  # TODO -- cap + 1?
             if num in alloc and num in ifree:
                 print(f"ALLOCATED INODE {num} ON FREELIST")
+                error_flag = True
             if num not in alloc and num not in ifree:
                 print(f"UNALLOCATED INODE {num} NOT ON FREELIST")
+                error_flag = True
 
     def check_directories(dirents_t, alloc):
+        global error_flag
         for entry in dirents_t:
             if entry.name != "'.'" and entry.name != "'..'":
                 parent_dir[entry.inode] = entry.parent_inode
             if entry.inode > cap or entry.inode <= 0:
-                print(
-                    f"DIRECTORY INODE {entry.parent_inode} NAME {entry.name} INVALID NODE {entry.inode}")
+                print(f"DIRECTORY INODE {entry.parent_inode} NAME {entry.name} INVALID NODE {entry.inode}")
+                error_flag = True
             elif entry.inode not in alloc:
-                print(
-                    f"DIRECTORY INODE {entry.parent_inode} NAME {entry.name} UNALLOCATED INODE {entry.inode}")
+                print(f"DIRECTORY INODE {entry.parent_inode} NAME {entry.name} UNALLOCATED INODE {entry.inode}")
+                error_flag = True
             else:
                 inode = entry.inode
                 if inode not in link_count:
@@ -109,20 +187,24 @@ def scan_inodes(inodes, dirents, first, cap):
                     link_count[inode] += 1
 
     def check_links(inodes_t, counts):
+        global error_flag
         for inode in inodes_t:
             num = inode.num
             if counts[num] != inode.link_count:
-                print(
-                    f"INODE {num} HAS {counts[num]} LINKS BUT LINKCOUNT IS {inode.link_count}")
+                print(f"INODE {num} HAS {counts[num]} LINKS BUT LINKCOUNT IS {inode.link_count}")
+                error_flag = True
 
     def check_curr_parent_dir(dirents_t):
+        global error_flag
         for entry in dirents_t:
             if entry.name == "'.'" and entry.inode != entry.parent_inode:
                 print(f"DIRECTORY INODE {entry.parent_inode} \
                 NAME '.' LINK TO INODE {entry.inode} SHOULD BE {entry.parent_inode}")
+                error_flag = True
             if entry.name == "'..'" and parent_dir[entry.parent_inode] != entry.inode:
                 print(f"DIRECTORY INODE {entry.parent_inode} \
                 NAME '..' LINK TO INODE {entry.inode} SHOULD BE {entry.parent_inode}")
+                error_flag = True
 
     check_freelist(inodes, allocated)
     check_directories(dirents, allocated)
@@ -141,42 +223,54 @@ def main():
 
     # Load CSV
     try:
-        csv_f = open(argv[1], 'r')
+        csv_file = open(argv[1], 'r')
     except OSError:
         sys.stderr.write("Error: Unable to open specified file.")
-        exit(2)
+        exit(1)
 
-    superblock, group = None, None
+    # Create variables
+    superblock = None
+    group = None
     inodes = []
     dirents = []
     indirects = []
 
-    reader = csv.reader(csv_f, delimiter=',')
+    # Set up CSV
+    reader = csv.reader(csv_file, delimiter=',')
     for row in reader:
-        elem = row[0]
-        if elem == "SUPERBLOCK":
-            superblock = Superblock(row)
-        elif elem == "GROUP":
-            group = Group(row)
-        elif elem == "BFREE":
+        element = row[0]
+        if element == "SUPERBLOCK":
+            superblock = Superblock(row)  # only one superblock
+        elif element == "GROUP":
+            group = Group(row)  # only one group
+        elif element == "BFREE":
             bfree.append(int(row[1]))
-        elif elem == "IFREE":
+        elif element == "IFREE":
             ifree.append(int(row[1]))
-        elif elem == "INODE":
+        elif element == "INODE":
             inodes.append(Inode(row))
-        elif elem == "DIRENT":
+        elif element == "DIRENT":
             dirents.append(Dirent(row))
-        elif elem == "INDIRECT":
+        elif element == "INDIRECT":
             indirects.append(Indirect(row))
         else:
             sys.stderr.write("Error: Inconcistency in CSV")
-            sys.exit(1)
+            exit(1)
 
     # TODO -- if not superblock or not group, error?
 
-    scan_blocks()
-    scan_inodes(inodes, dirents, superblock.first_inode, superblock.num_inodes)
+    initial_block = int(group.inode_table)
+    initial_block += int(math.ceil(superblock.inode_size * group.inode_per_group / superblock.block_size))
+    num_blocks = superblock.num_blocks
+
+    # TODO -- change function names
+    check_blocks(inodes, dirents, indirects, initial_block, num_blocks)
+    check_inodes(inodes, dirents, superblock.first_inode, superblock.num_inodes)
 
 
 if __name__ == "__main__":
     main()
+    if error_flag:
+        exit(2)
+    else:
+        exit(0)
