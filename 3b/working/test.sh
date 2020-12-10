cb1001086cd51cb1d8bb6146d2bceb6cc463b1da
William Randall <wrandall1000@gmail.com>
2020-12-10 05:00:13 -0600

    updated test.sh

--- 3b/working/test.sh
+++ 3b/working/test.sh
@@ -5,16 +5,32 @@ do
 	./lab3b $INPUT_FILE > test/$i.txt
 done
 
+if ! ./lab3b test/trivial.csv > test/23.txt
+then
+  echo "trivial.csv passed"
+else
+  echo "trivial.csv failed"
+  echo "===correct==="
+  echo "NO OUTPUT"
+  echo "===incorrect==="
+  cat test/23.txt
+  echo "===end==="
+fi
+
 for i in {1..22}
 do
 	ERROR_FILE=test/P3B-test_$i.err
 	OUTPUT_FILE=test/$i.txt
 	DIFF_FILE=test/$i.diff
-	diff -u $ERROR_FILE $OUTPUT_FILE > $DIFF_FILE
-	if [ $? -eq 0 ]
+	if ! diff -u $ERROR_FILE $OUTPUT_FILE > $DIFF_FILE
 	then
 		echo "test $i passed"
 	else
 		echo "test $i failed"
+		echo "===correct==="
+		cat $ERROR_FILE
+		echo "===incorrect==="
+		cat $OUTPUT_FILE
+		echo "===end==="
 	fi
 done
